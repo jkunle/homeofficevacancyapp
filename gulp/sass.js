103e2c5f53471bca95ae21e425a0f771e8a797cb
olakunle_jegede <jkunle@googlemail.com>
2016-08-11 06:38:01 +0100

    added gulpfile Broke out gulping into seperate files to enhance readability

---
+++ gulp/sass.js
@@ -0,0 +1,17 @@
+(function () {
+    var gulp = require('gulp'),
+        sass = require('gulp-sass'),
+        sourcemaps = require('gulp-sourcemaps');
+
+    module.exports = function sassingassets (done) {
+        gulp.src('assets/style.scss')
+            .pipe(sourcemaps.init())
+            .pipe(sass({
+                outputStyle: 'expanded'
+            }))
+            .on('error', sass.logError)
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('builds/css'))
+            .on('end', done);
+    }
+})();
