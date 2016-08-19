384a3a4615a9ccabfde981364376a3993efdb357
olakunle_jegede <jkunle@googlemail.com>
2016-08-19 12:11:45 +0100

    seperated out DataAccess from data retrieval

--- services/database.js
+++ services/database.js
@@ -1,52 +1,56 @@
 // Require the nedb module
-var Datastore = require('nedb') //using this cause it feels like mongodb
-var store = require('./data.json');
-// create instances representing a list of applicants and a list of their profiles
-var _applicants = new Datastore({ filename:  './database/applicants', autoload: true });
-var _profiles = new Datastore({ filename:  './database/applicant_profiles', autoload: true });
+'use strict';
+function DataAccess(repo) {
 
-function DaO() {
-    // helper function to generate email that matches first.last@domain.com
-    function generateCorrelatedEmail(first , last, email){
-        return  `${first}.${last}${email.substring(email.indexOf('@'), email.length)}`;
-    }
-	
-    function getAllApplicants(callback) {
-		_applicants.find({}, function (err, result) {
-			return callback(err, result);
-		});
-    }
-    //Insert applicant and profile from data.json into two seperate collections
-    // managed by this service. This is executed on every start up of the application
-    // however because of th unique contraint, subsequent writes will fail. Thus maintaining
-    // the same data set.
-	(function loadApplicantsfromDataDotJSON (req, res) {
-        
-        let applicants = _applicants;
-        let profiles = _profiles
-        
-        for (let data of store) {
-                let {number, firstname, lastname, date} = data; 
-                let {email,contact_number,nationality,work_experience} = data;
-                var profileMail = generateCorrelatedEmail(firstname, lastname, email);
-                // insert into applicants collection
-                applicants.insert( {number,firstname, lastname, date});
-                // insert into profiles collection
-                profiles.insert({number, profileMail, contact_number,nationality,work_experience});
-        }
-        // Create a "unique" index for the applicant number and profile number
-        applicants.ensureIndex({ fieldName: 'number', unique: true });
-	    profiles.ensureIndex({ fieldName: 'number', unique: true });
+    var _repo = repo;
 
-	})(_applicants, _profiles);
-    
+    function getAllApplicants(param, callback) {
+
+        _repo.applicants.find(param.query)
+            .sort(param.sort).skip(param.skip)
+            .limit(param.limit)
+            .exec(function (err, result) {
+                return callback(err, result);
+            });
+    }
+
+    function getProfiles(callback) {
+
+        _repo.profiles.find({}, function (err, result) {
+            return callback(err, result);
+        });
+
+    }
+
+    function getApplicantProfile(no, callback) {
+
+
+        _repo.applicants.find({ 'number': no }, function (err, app_result) {
+
+            let {number, firstname, lastname} = app_result[0];
+
+            _repo.profiles.find({ number: parseInt(no) }, function (err, prof_result) {
+
+                let {profileMail, contact_number, nationality, work_experience} = prof_result[0];
+
+                return callback(err, {
+                    number, firstname, lastname, profileMail,
+                    contact_number, nationality, work_experience
+                });
+
+            });
+
+        });
+
+    }
    
-
-	return {
-		getAllApplicants: getAllApplicants
-	}
+    return {
+        getAllApplicants: getAllApplicants,
+        getProfiles: getProfiles,
+        getApplicantProfile: getApplicantProfile,
+    }
 }
 
-module.exports = DaO;
+module.exports = DataAccess;
 
-	
+
